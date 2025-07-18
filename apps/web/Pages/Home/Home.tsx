6c036619d3d50afea7536a469d8fe9f30305e898
ReneNieto <209809806+rnieto-g360@users.noreply.github.com>
2025-07-18 13:15:13 -0600

    removed call

--- apps/web/Pages/Home/Home.tsx
+++ apps/web/Pages/Home/Home.tsx
@@ -1,13 +1,8 @@
-import { useQuery } from "@tanstack/react-query"
 import { Chart } from "../../src/components/Chart/Chart"
 import DateForm from "../../src/components/DateForm/DateForm"
 import Table from "../../src/components/Table/Table"
 import { DarkModeToggle } from "../../src/components/ui/darkModeToggle/DarkModeToggle"
-import { getWordFrequencyCommitsWordFrequencyGetOptions } from "../../src/client/@tanstack/react-query.gen"
 export default function Home() {
-	const { data } = useQuery(getWordFrequencyCommitsWordFrequencyGetOptions())
-
-	console.log(data)
 	return (
 		<>
 			<header className="flex items-center justify-between p-4">
